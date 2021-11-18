914565b113a5daecbc904ee8b54c8e0009429283
Rhuarhri Cordon <rhuarhri.cordon@3squared.com>
2021-11-18 16:25:21 +0000

    additional setup mainly of hilt and realm

---
+++ app/src/main/java/com/three_squared/rhuarhri_induction/DependencyBase.kt
@@ -0,0 +1,8 @@
+package com.three_squared.rhuarhri_induction
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DependencyBase : Application() {
+}
---
+++ app/src/main/java/com/three_squared/rhuarhri_induction/dependency_injection/Dependencies.kt
@@ -0,0 +1,30 @@
+package com.three_squared.rhuarhri_induction.dependency_injection
+
+import android.content.Context
+import com.three_squared.rhuarhri_induction.DependencyBase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.realm.RealmConfiguration
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class Dependencies {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app : Context) : DependencyBase {
+        return app as DependencyBase
+    }
+
+    @Singleton
+    @Provides
+    fun providesRealmConfig() : RealmConfiguration {
+        return RealmConfiguration.Builder()
+            .schemaVersion(1)
+            .build()
+    }
+}
