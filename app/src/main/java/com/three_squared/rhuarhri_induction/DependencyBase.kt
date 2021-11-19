9f55a7b1c32529b74f33ac82557c4cdfb06648bc
Rhuarhri Cordon <rhuarhri.cordon@3squared.com>
2021-11-19 09:19:42 +0000

    fixing hilt plugin error

--- app/src/main/java/com/three_squared/rhuarhri_induction/DependencyBase.kt
+++ app/src/main/java/com/three_squared/rhuarhri_induction/DependencyBase.kt
@@ -1,9 +1,9 @@
 package com.three_squared.rhuarhri_induction
 
-/*
+
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class DependencyBase : Application() {
-}*/
+}
--- app/src/main/java/com/three_squared/rhuarhri_induction/dependency_injection/Dependencies.kt
+++ app/src/main/java/com/three_squared/rhuarhri_induction/dependency_injection/Dependencies.kt
@@ -1,8 +1,11 @@
 package com.three_squared.rhuarhri_induction.dependency_injection
 
+import android.content.Context
+import com.three_squared.rhuarhri_induction.DependencyBase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.realm.RealmConfiguration
 import javax.inject.Singleton
@@ -11,12 +14,12 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class Dependencies {
 
-    /*@Singleton
+    @Singleton
     @Provides
     fun provideApplication(@ApplicationContext app : Context) : DependencyBase {
 
         return app as DependencyBase
-    }*/
+    }
 
     @Singleton
     @Provides
