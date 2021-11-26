28eff077ff11c7c3dd8e01db30c6ba5591b83e71
Rhuarhri Cordon <rhuarhri.cordon@3squared.com>
2021-11-26 16:42:47 +0000

    adding token in header in order to authenticate github api use. This also means that the app does not work out of the box. This is because the app needs a personal access token from github which for security reason it not available in this repository.

--- app/src/main/java/com/three_squared/rhuarhri_induction/online/RetroFitInterface.kt
+++ app/src/main/java/com/three_squared/rhuarhri_induction/online/RetroFitInterface.kt
@@ -3,18 +3,22 @@ package com.three_squared.rhuarhri_induction.online
 import com.three_squared.rhuarhri_induction.online.data.CommitOnline
 import com.three_squared.rhuarhri_induction.online.data.RepositoryOnline
 import com.three_squared.rhuarhri_induction.online.data.UserOnline
+import com.three_squared.rhuarhri_induction.online.privateTokens.githubToken
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface RetroFitInterface {
+    @Headers("Authorization: token $githubToken")
     @GET("users/{name}")
     fun getUser(@Path(value = "name") name : String): Call<UserOnline>
 
+    @Headers("Authorization: token $githubToken")
     @GET("users/{name}/repos")
     fun getRepositories(@Path(value = "name") name : String): Call<List<RepositoryOnline>>
 
-
+    @Headers("Authorization: token $githubToken")
     @GET("repos/{name}/{repository}/commits")
     fun getCommits(@Path(value = "name") name : String,
                    @Path(value = "repository") repositoryName : String)
